02e55a3cb6eb6977ce6a1e211c54c6b1304fc63f
princepatrick <princepatrickanand1996@gmail.com>
2023-04-23 12:46:59 -0400

    Project_1_Madlibs

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+
+adjective = input("Enter an adjective: ")
+verb1 = input("Enter an action in past tense: ")
+verb2 = input("Enter an action in past tense: ")
+person = input("Enter the name of a character you think of right now: ")
+
+print(f'The {adjective} boy had {verb1} after he had {verb2} {person}')
